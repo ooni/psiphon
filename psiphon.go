9af3b55437b359c58715580733975373be54eb5e
Simone Basso <bassosimone@gmail.com>
2020-11-05 11:46:40 +0100

    fix: add toplevel go file
    Hopefully this is enough to trigger remove updates?

---
+++ psiphon.go
@@ -0,0 +1,5 @@
+// Package psiphon vendors psiphon-tunnel-core and most of its
+// dependencies. To this end, we use the oopsi subpackage. We
+// construct this repository using the client-staging branch of
+// github.com/Psiphon-Labs/psiphon-tunnel-core.
+package psiphon
